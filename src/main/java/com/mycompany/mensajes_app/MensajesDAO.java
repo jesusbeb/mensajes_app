d689e724c88560601c6fff03c2af5bfa376f6027
jesusbeb <jbeltranb@outlook.com>
2025-06-16 17:35:12 -0600

    Creacion de Mensajes en Base de Datos

--- src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
@@ -1,10 +1,43 @@
 
 package com.mycompany.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/* Las clases DAO son las encargadas de hacer la conexion con la BD
+*/
 public class MensajesDAO {
     
-    public static void CrearMensajeDB(Mensajes mensaje){
+    public static void crearMensajeDB(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
         
+        //Con el metodo try nos conectamos a la BD
+        try( Connection conexion = db_connect.get_connection() ){
+            //Objeto de Sentencia Preparada
+            PreparedStatement ps = null;
+            
+            //try para insertar los datos en la BD
+            try{
+                //Creamos la consulta para insertar los datos
+                //A ps le pasamos la sentencia SQL para insertar los datos
+                String query = "INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?,?)";
+                ps = conexion.prepareStatement(query);
+                //El mensaje consta de cuatro atributos, pero solo pasamos dos, ya 
+                //que los otros dos son automaticos por la BD (id y fecha)
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                //Ejecuta la sentencia a la BD
+                ps.executeUpdate();
+                System.out.println("Mensaje creado");
+                
+            } catch(SQLException ex){ //En caso de no crear el mensaje
+                System.out.println("ex");
+            }
+        } catch(SQLException e){ //Por si alguna razon no nos conectamos a la BD
+            System.out.println(e);
+        } 
+            
     }
     
     
--- src/main/java/com/mycompany/mensajes_app/mensajesService.java
+++ src/main/java/com/mycompany/mensajes_app/mensajesService.java
@@ -1,10 +1,27 @@
 
 package com.mycompany.mensajes_app;
 
+import java.util.Scanner;
+
+/* Las clases service son las encargadas de trabajar con los datos ingresados por 
+el usuario
+*/
 public class mensajesService {
     
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Escribe tu mensaje: ");
+        String mensaje = sc.nextLine();     
+        System.out.println("Escribe tu nombre: ");
+        String nombre = sc.nextLine();
+        
+        //Creamos el objeto mensaje
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        //Enviamos el objeto mensaje a la capa DAO
+        MensajesDAO.crearMensajeDB(registro);
     }
     
     
